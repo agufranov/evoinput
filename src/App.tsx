5a14d1ed568a463b2c2a23ddc99e51e19c3cdbb8
agufranov <al.gufranov@gmail.com>
2025-08-01 18:12:47 +0300

    init app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import style from "./App.module.css";
+
+function App() {
+  return (
+    <main className={style.root}>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </main>
+  );
+}
+
+export default App;
