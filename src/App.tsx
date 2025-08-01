d070a77dfbd89305a4741df84851e0caba35c79d
agufranov <al.gufranov@gmail.com>
2025-08-01 20:43:38 +0300

    add styling and variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,26 @@ import style from "./App.module.css";
 import { ShortcutInput } from "./components/ShortcutInput";
 
 function App() {
-  const [value, setValue] = useState<string | undefined>("Control+Shift+A");
+  const [value1, setValue1] = useState<string | undefined>("Control+Shift+A");
+  const [value2, setValue2] = useState<string | undefined>();
   return (
     <main className={style.root}>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <input />
-      {value}
-      <ShortcutInput value={value} onChange={setValue} />
-      <ShortcutInput />
+      <div>
+        <h1>With initial value</h1>
+        <div className={style.row}>
+          <ShortcutInput value={value1} onChange={setValue1} />
+          <button onClick={() => setValue1(undefined)}>Clear</button>
+        </div>
+        <div className={style.value}>Value: "{value1}"</div>
+      </div>
+      <div>
+        <h1>Without initial value</h1>
+        <div className={style.row}>
+          <ShortcutInput onChange={setValue2} />
+          <button onClick={() => setValue2(undefined)}>Clear</button>
+        </div>
+        <div className={style.value}>Value: "{value2}"</div>
+      </div>
     </main>
   );
 }
